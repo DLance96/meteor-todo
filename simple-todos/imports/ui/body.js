da9beb14a62a307440c63af1122b8ab2686b7cd9
David Lance <dcl71@case.edu>
2016-05-25 09:13:41 -0400

    Added task database via Mongo and form to add tasks

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,11 +1,29 @@
 import { Template } from 'meteor/templating';
-
+import { Tasks } from '../api/tasks.js';
 import './body.html';
 
 Template.body.helpers({
-  tasks: [
-  { text: 'This is task 1' },
-  { text: 'This is task 2' },
-  { text: 'This is task 3' },
+  tasks() {
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
 
-]});
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default value from being added to the db
+    event.preventDefault();
+
+    // get value from the form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // insert task into the db
+    Tasks.insert({
+      text,
+      createdAt: new Date(), //current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
